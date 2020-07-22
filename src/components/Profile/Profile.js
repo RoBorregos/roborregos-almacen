62279a79515683567806165539f0b1ba319767b7
Jamir <A00826275@itesm.mx>
2020-07-22 15:58:25 -0600

    Modal that shows reserved components created

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,10 @@
 import './Profile.css';
 
 import { Button, Col, Row } from 'react-bootstrap';
+import { Modal, ModalBody } from 'react-bootstrap';
 import React, { Component } from 'react';
 
+import ModalHeader from 'react-bootstrap/ModalHeader';
 import { connect } from 'react-redux';
 import members from '../../data/members.json';
 
@@ -13,9 +15,24 @@ class Profile extends Component {
         this.member = members.members[0]
         this.mock_reservations = props.mock_reservations;
 
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+
         this.loadMember = this.loadMember.bind(this);
         this.loadReservations = this.loadReservations.bind(this);
+
+        this.state = {
+            show: false
+        }
     }
+
+    handleShow() {
+        this.setState({ show: true })
+      }
+    
+      handleClose() {
+        this.setState({ show: false })
+      }
 
     loadMember() {
         return (
@@ -80,7 +97,16 @@ class Profile extends Component {
                     </Col>
                 </Col>
                 <Col>
-                    <Button> Click here to return components </Button>
+                    <Button onClick={this.handleShow}> Click here to return components </Button>
+                    <Modal
+                    show={this.state.show}
+                    onHide={this.handleClose}
+                    >
+                        <ModalHeader className='cart-head' closeButton><h3>Reserved Components</h3></ModalHeader>
+                        <ModalBody>
+                        Here goes the list of reserved components
+                        </ModalBody>
+                    </Modal>
                 </Col>
             </div>
         );
