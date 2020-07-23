a2ea5dcbbc8d3381d2d00a69193484bc0db7705e
Jamir <A00826275@itesm.mx>
2020-07-23 14:00:55 -0600

    Separate instatiation of returning modal

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,9 @@
 import './Profile.css';
 
-import { Button, Col, Row } from 'react-bootstrap';
-import { Modal, ModalBody } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import React, { Component } from 'react';
-import { addQuantity, clearCart, removeItem, subtractQuantity, types } from '../../scripts/cartReducer';
 
-import ActiveComponents from '../../data/active_components.json';
-import ModalHeader from 'react-bootstrap/ModalHeader';
+import ReturningModal from './ReturningModal/ReturningModal';
 import { connect } from 'react-redux';
 import members from '../../data/members.json';
 
@@ -16,26 +13,13 @@ class Profile extends Component {
 
         this.member = members.members[0]
         this.mock_reservations = props.mock_reservations;
-        this.active_components = ActiveComponents;
-
-        this.handleShow = this.handleShow.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-
         this.loadMember = this.loadMember.bind(this);
         this.loadReservations = this.loadReservations.bind(this);
-        this.loadReserved = this.loadReserved.bind(this);
-        this.state = {
-            show: false
-        }
     }
 
-    handleShow() {
-        this.setState({ show: true })
-      }
-    
-      handleClose() {
-        this.setState({ show: false })
-      }
+    handleModalState() {
+        this.modalEnabled = !this.modalEnabled;
+    }
 
     loadMember() {
         return (
@@ -73,42 +57,6 @@ class Profile extends Component {
         return componentsList;
     }
 
-    loadReserved() {
-
-        let componentsList = [];
-        let countComponents = 0;
-        for (let reserved = 0; reserved < this.active_components.reservations.length; reserved++) {
-            if (this.active_components.reservations[reserved].memberID !== this.member.memberID) continue;
-            let componentsArray = this.active_components.reservations[reserved].activeComponents;
-            for (let i = 0; i < componentsArray.length; i++) {
-                let style = (countComponents % 2 === 0) ? "oddRow" : "evenRow";
-                componentsList.push(
-                    <Row className={ style }>
-                            <Col xs='6'>
-                                <Col className="reservations">{componentsArray[i].componentID}</Col>
-                            </Col>
-                            <Col xs='6'>
-                                <Col xs='3' className='col-pd hor-center'>
-                                    <Button className='subt-button'>-</Button>
-                                </Col>
-                                <Col xs='3' className='item-counter col-pd ver-center hor-center'>
-                                    <div className="input-group-field">{componentsArray[i].quantity}</div>
-                                </Col>
-                                <Col xs='3' className='col-pd hor-center'>
-                                    <Button className='add-button'>+</Button>
-                                </Col>
-                                <Col xs='3' className='col-pd hor-center'>
-                                    <Button className='rem-button'>x</Button>
-                                </Col>
-                            </Col>
-                    </Row>
-                );
-                countComponents++;
-            }
-        }
-        return componentsList;
-    }
-
     render() {
         return (
             <div className="profile_container">
@@ -134,31 +82,7 @@ class Profile extends Component {
                     </Col>
                 </Col>
                 <Col>
-                    <Button onClick={this.handleShow}> Click here to return components </Button>
-                    <div onClick={e => e.stopPropagation()}>
-                        <Modal
-                        show={this.state.show}
-                        onHide={this.handleClose}
-                        >
-                            <ModalHeader className='returning_head' closeButton><h3>Reserved Components</h3></ModalHeader>
-                            <ModalBody>
-                                <Row className="justify-content-center">
-                                        <Col className="headers justify-content-center">
-                                            <h5>Component</h5>
-                                        </Col>
-                                        <Col className="headers justify-content-center">
-                                            <h5>Quantity</h5>
-                                        </Col>
-                                </Row>
-                                {this.loadReserved()}
-                                <Row className="justify-content-center">
-                                    <Button className='checkout-button'>
-                                        Return components
-                                    </Button>  
-                                </Row>
-                            </ModalBody>
-                        </Modal>
-                    </div>
+                    <ReturningModal memberID={this.member.memberID} state = { this.modalEnabled } />  
                 </Col>
             </div>
         );
