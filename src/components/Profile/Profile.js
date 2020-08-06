db74c306dea3f8e8d12262b6a90881af91dcc49e
Jamir <A00826275@itesm.mx>
2020-08-05 23:07:43 -0600

    Special case for when there are not returned components or reservations

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -27,44 +27,94 @@ class Profile extends Component {
     }
 
     loadReturned() {
-        return(
-            this.returned_components.map((component, index) => {
-                return (
-                    <div key={ index }>
-                        <Row className={ (index % 2 === 0 ? "oddRow" : "evenRow") }>
-                            <Col className="reservations">{component.dateReturned}</Col>
-                            <Col className="reservations">{component.componentID}</Col>
-                            <Col className="reservations">{component.quantity}</Col>
+        if(this.user_index !== -1){
+            return (
+                <div>
+                    <Col>
+                        <Row className="first-row justify-content-center">
+                            <Col className="justify-content-center">
+                                <h2>Date</h2>
+                            </Col>
+                            <Col className="justify-content-center">
+                                <h2>Component</h2>
+                            </Col>
+                            <Col className="justify-content-center">
+                                <h2>Quantity</h2>
+                            </Col>
                         </Row>
-                    </div>
-                )
-            })
-        )
+                    </Col>
+                    <Col>
+                        {
+                            this.returned_components.map((component, index) => {
+                                return (
+                                    <div key={ index }>
+                                        <Row className={ (index % 2 === 0 ? "oddRow" : "evenRow") }>
+                                            <Col className="reservations">{component.dateReturned}</Col>
+                                            <Col className="reservations">{component.componentID}</Col>
+                                            <Col className="reservations">{component.quantity}</Col>
+                                        </Row>
+                                    </div>
+                                )
+                            })
+                        }
+                    </Col>
+                </div>
+            )
+        } else {
+            return (
+                <h3>You have not returned any component</h3>
+            )
+        }
     }
 
     loadReservations() {
-        return(
-            this.mock_reservations.map((eachReservation) => {
-                if ( eachReservation.memberID === this.memberID ) {
-                    return (
-                        <div key={eachReservation.reservation_key}>
-                            { eachReservation.reservation.map((eachComponent, index) => {
-                            return (
-                                <div key={ index }>
-                                    <Row className={ (index % 2 === 0 ? "oddRow" : "evenRow") }>
-                                        <Col className="reservations">{eachReservation.date}</Col>
-                                        <Col className="reservations">{eachComponent.componentID}</Col>
-                                        <Col className="reservations">{eachComponent.quantity}</Col>
-                                    </Row>
-                                </div>
-                            )
-                            })}   
-                        </div>
-                    )
-                }
-                return null;
-            })
-        )
+        if(this.mock_reservations.findIndex((each) =>  each.memberID === this.memberID) !== -1){
+            return(
+                <div>
+                    <Col>
+                        <Row className="first-row justify-content-center">
+                            <Col className="justify-content-center">
+                                <h2>Date</h2>
+                            </Col>
+                            <Col className="justify-content-center">
+                                <h2>Component</h2>
+                            </Col>
+                            <Col className="justify-content-center">
+                                <h2>Quantity</h2>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col>
+                        {
+                            this.mock_reservations.map((eachReservation) => {
+                            if( eachReservation.memberID === this.memberID ) {
+                                return (
+                                    <div key={eachReservation.reservation_key}>
+                                        { eachReservation.reservation.map((eachComponent, index) => {
+                                        return (
+                                            <div key={ index }>
+                                                <Row className={ (index % 2 === 0 ? "oddRow" : "evenRow") }>
+                                                    <Col className="reservations">{eachReservation.date}</Col>
+                                                    <Col className="reservations">{eachComponent.componentID}</Col>
+                                                    <Col className="reservations">{eachComponent.quantity}</Col>
+                                                </Row>
+                                            </div>
+                                        )
+                                        })}   
+                                    </div>
+                                )
+                            }
+                            return null;
+                            })
+                        }
+                    </Col>
+                </div>
+            )
+        } else {
+            return (
+                <h3>You have not done any reservations</h3>
+            )
+        }
     }
 
     render() {
@@ -74,43 +124,13 @@ class Profile extends Component {
                     <h1>Your Reservations</h1>
                 </Col>
                 <Col>
-                    <Col>
-                        <Row className="first-row justify-content-center">
-                            <Col className="justify-content-center">
-                                <h2>Date</h2>
-                            </Col>
-                            <Col className="justify-content-center">
-                                <h2>Component</h2>
-                            </Col>
-                            <Col className="justify-content-center">
-                                <h2>Quantity</h2>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col>
-                        {this.loadReservations()}
-                    </Col>
+                    { this.loadReservations() }
                 </Col>
                 <Col >
                     <h1>Your returned components</h1>
                 </Col>
                 <Col>
-                    <Col>
-                        <Row className="first-row justify-content-center">
-                            <Col className="justify-content-center">
-                                <h2>Date</h2>
-                            </Col>
-                            <Col className="justify-content-center">
-                                <h2>Component</h2>
-                            </Col>
-                            <Col className="justify-content-center">
-                                <h2>Quantity</h2>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col>
-                        {this.loadReturned()}
-                    </Col>
+                    { this.loadReturned() }
                 </Col>
                 <Col>
                     <ReturningModal memberID={ this.memberID } />  
